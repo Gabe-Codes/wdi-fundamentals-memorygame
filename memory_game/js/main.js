978c3483a9c9d499595a78234dc7956ccff4c61b
Gabriel Russell <gabrielrussell@Gabriels-MacBook-Air.local>
2019-12-01 15:00:23 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,44 +1,74 @@
-var cards = [
-{
-  rank: "queen",
-  suit: "hearts",
-  cardImage: "images/queen-of-hearts.png"
-},
-{
-  rank: "queen",
-  suit: "diamonds",
-  cardImage: "images/queen-of-diamonds.png"
-},
-{
-  rank: "king",
-  suit: "hearts",
-  cardImage: "images/king-of-hearts.png"
-},
-{
-  rank: "king",
-  suit: "diamonds",
-  cardImage: "images/king-of-diamonds.png"
-}
+var cards = [{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
 ];
+
 var cardsInPlay = [];
+var streak = 0;
 
 function checkForMatch() {
-  if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
-    } else {
-      alert("Sorry, try again.");
-    }
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    streak += 1;
+    cardsInPlay = [];
+    document.getElementById("streak").innerHTML = streak;
+    alert("You found a match!");
+    clearBoard();
+  } else {
+    streak = 0;
+    cardsInPlay = [];
+    document.getElementById("streak").innerHTML = streak;
+    alert("Sorry, try again.");
+    clearBoard();
   }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
-  cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
-  checkForMatch();
+  cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', (cards[cardId].cardImage));
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+
+function clearBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var board = document.getElementById('game-board');
+    var boardCards = board.getElementsByTagName('img');
+    boardCards[i].setAttribute('src', 'images/back.png');
+    boardCards[i].setAttribute('data-id', i);
+    boardCards[i].addEventListener("click", flipCard);
+  }
+}
+
+createBoard();
